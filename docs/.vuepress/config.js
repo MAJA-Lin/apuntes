d31bf6c83b260af57910c764fd3e31981b66c44f
Scott Lin <josh.al.carrer@gmail.com>
2019-04-04 13:40:37 +0800

    Initialize the project with vue-press

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    base: "/apuntes/",
+    title: 'Notes',
+    description: 'Scott Lin\'s notes',
+    head: [
+        ['link', {
+            rel: 'icon', href: '/favicon.ico'
+        }]
+    ],
+
+    plugins: {
+        // 'clean-urls': {
+        //     normalSuffix: '',
+        //     indexSuffix: ''
+        // }
+    }
+}
