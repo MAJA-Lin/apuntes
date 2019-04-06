6db9e042ab5b9bda9afe718bd3a57edcf7b7cbd8
Scott Lin <josh.al.carrer@gmail.com>
2019-04-07 01:58:32 +0800

    Add basic css file and md line number

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,17 +1,30 @@
 module.exports = {
-    base: "/apuntes/",
     title: 'Notes',
     description: 'Scott Lin\'s notes',
+    base: "/apuntes/",
     head: [
         ['link', {
-            rel: 'icon', href: '/favicon.ico'
-        }]
+            rel: 'icon',
+            href: '/favicon.ico'
+        }],
+        ['link', {
+            rel: 'preconnect',
+            href: 'https://fonts.gstatic.com/',
+            crossorigin: 'anonymous'
+        }],
+        ['link', {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+        }],
+        ['link', {
+            rel: 'stylesheet',
+            href: 'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+            integrity: 'sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU',
+            crossorigin: 'anonymous'
+        }],
     ],
 
-    plugins: {
-        // 'clean-urls': {
-        //     normalSuffix: '',
-        //     indexSuffix: ''
-        // }
+    markdown: {
+        lineNumbers: true
     }
 }
